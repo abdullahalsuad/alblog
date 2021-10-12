c782fdd4a7a8777b12b41ca942df99bab526582d
unknown <suadabdullahal@gmail.com>
2021-10-12 14:00:27 +0600

    13  added

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,14 +1,14 @@
-const BlogList = ({blogs,name}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     // const blogs = props.blogs; 
     // const name = props.name;
-
     return ( 
         <div className="blog-list">
-            <h1>Hi {name} !</h1>
+            <h1>{title}</h1>
             {blogs.map((blog) =>(
            <div className="blog-preview" key={blog.id}>
               <h2>{blog.title}</h2>
               <p>Written by {blog.author}</p>
+              <button onClick={()=> handleDelete(blog.id)}>Delete Blog</button>
            </div>   
        ))} 
         </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,11 +7,17 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-    { title: 'Web dev ', body: 'lorem ipsum...', author: 'suad', id: 4 }
   ])
+
+  const handleDelete = (id) =>{
+    const newBlogs = blogs.filter(blog => blog.id !== id );
+    setBlogs(newBlogs);
+    
+  }
+
     return (
       <div className="home">
-       <BlogList blogs={blogs} name="Suad"/>
+       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
       </div>
     );
   }
