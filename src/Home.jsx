572fbcb2999869073b18fbf834b2f883cae256a9
suad <suadabdullahal@gmail.com>
2021-12-15 19:24:49 +0600

    custom hoock

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+
 const Home = () => {
-
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ])
-
-  const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blog => blog.id !== id );
-    setBlogs(newBlogs);
-    
-  }
-  useEffect(() => {
-    console.log("run");
-  },)
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
     return (
       <div className="home">
-       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-      </div>
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
     );
   }
    
