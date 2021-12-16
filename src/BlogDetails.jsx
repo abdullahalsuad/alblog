1b66b6bf1c48b58232c02e1cac8357f038c44c4d
suad <suadabdullahal@gmail.com>
2021-12-17 00:04:06 +0600

    blogdetails page loadeing animation added

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { Spinner } from "react-bootstrap";
 
 const BlogDetails = () => {
 
@@ -8,7 +9,20 @@ const BlogDetails = () => {
 
     return (  
         <div className="blog-details">
-        { isPending && <div>Loading...</div> }
+        { isPending && 
+        
+        <div className="d-flex justify-content-center">
+        <Spinner animation="grow" variant="primary" className="m-2"/>
+        <Spinner animation="grow" variant="secondary" className="m-2"/>
+        <Spinner animation="grow" variant="success" className="m-2"/>
+        <Spinner animation="grow" variant="danger" className="m-2"/>
+        <Spinner animation="grow" variant="warning" className="m-2"/>
+        <Spinner animation="grow" variant="info" className="m-2"/>
+        <Spinner animation="grow" variant="light" className="m-2"/>
+        <Spinner animation="grow" variant="dark" className="m-2"/>
+      </div>
+        
+        }
         { error && <div>{ error }</div> }
         { blog && (
           <article>
